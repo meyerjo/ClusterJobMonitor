457f58b19ef403bbc89ca128411e823ab85b1a01
bojome <meyerjo@informatik.uni-freiburg.de>
2016-03-19 14:17:31 +0100

    * write files to database

--- sshmonitor/fileviews.py
+++ sshmonitor/fileviews.py
@@ -1,9 +1,12 @@
 import logging
 
+import transaction
 from pyramid.request import Request
 from pyramid.view import view_config
 from webob.multidict import NestedMultiDict
 
+from models import DBSession
+from models.FileMonitorModels import MonitoredFile
 from ssh.sshfilebrowser import SSHFileBrowser
 from sshmonitor import SSHBasedJobManager
 
@@ -22,12 +25,23 @@ class FileViews:
 
     @view_config(route_name='filemonitor_editor', renderer='templates/filemonitoring.pt')
     def filemonitoring(self):
+        log = logging.getLogger(__name__)
         if self._request.matchdict['modus'] == 'add':
             if self._request.matchdict['options'] == 'files':
-                print(self._request.params)
                 if self._request.params is not []:
+                    # TODO: add this information to the file
                     md5_enabled = True if 'withmd5' in self._request.params and self._request.params['withmd5'] == '0' else False
                     all_files = self._request.params.getall('file')
+                    print(all_files)
+                    with transaction.manager:
+                        for f in all_files:
+                            dbobj = MonitoredFile(f)
+                            DBSession.add(dbobj)
+                        DBSession.commit()
+                        log.info('Still have to delete files in this folder which are missing')
+                else:
+                    # TODO: delete files from database
+                    log.info('Have to delete files in this browser')
 
                 subreq = Request.blank(self._request.route_path('filebrowser'),
                                        POST=dict(folder=self._request.params['folder'],
@@ -41,7 +55,6 @@ class FileViews:
         log = logging.getLogger(__name__)
         ssh_holder = self._request.registry.settings['ssh_holder']
         ssh_jobmanager = SSHFileBrowser(ssh_holder)
-        print(self._request.params)
         folder = self._request.params['folder'] if 'folder' in self._request.params else '.'
         currentfolder = self._request.params['currentfolder'] if 'currentfolder' in self._request.params else '.'
         reference_type = self._request.params['pathdescription'] if 'pathdescription' in self._request.params else 'rel'
@@ -53,9 +66,12 @@ class FileViews:
                 folder_request = '{0}/{1}'.format(currentfolder, folder)
         elif reference_type == 'abs':
             if folder == '':
-                folder_request = '.'
+                folder_request = currentfolder
             else:
                 folder_request = folder
+        else:
+            log.info('Unknown request type {0}'.format(reference_type))
+            folder_request = '.'
 
         log.info('Requesting folder: {0}'.format(folder_request))
         output = ssh_jobmanager.get_folder_content(folder_request)
